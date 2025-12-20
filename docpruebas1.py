753e98473647f09d7665e710d2aadb50d2362940
Nikolab <nicolab.training3@gmail.com>
2025-12-20 00:15:29 -0300

    creación de ramas

--- docpruebas1.py
+++ docpruebas1.py
@@ -1,1 +1,14 @@
 print("primer code")
+
+
+#guia a seguir https://medium.com/@lorenzouriel/the-git-week-a-guide-to-branches-3daace9c145d
+
+#Creamos ramas
+
+#feature/main-vista
+#hotfix/vista-fix
+#release/v0.test
+#release/v1.0
+#prueba-merge1
+#rama-recic1
+#rama-recic2
